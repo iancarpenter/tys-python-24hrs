014ba2faf4181560d099d246053b2e960ce45cc5
Ian Carpenter <ianhcarpenter@gmail.com>
2014-11-23 21:12:05 +0000

    Added call_class.py and updated readme

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ tys-python-24hrs
 ================
 
 chapter 22 of the typ book
+
+Added call_class.py
---
+++ call_class.py
@@ -0,0 +1,10 @@
+from classes.some_class import MyClass
+
+def main():
+    c = MyClass(title="This is the title")
+    print c 
+	
+if __name__ == "__main__":
+    main()    
+	
+    	
